41f3a4b382d90f30f601bfc6308b86e4e158f0b4
Bradleyda932 <bradley.antholz@drake.edu>
2025-04-04 16:31:39 -0500

    Update Lab03.sql

--- Lab03.sql
+++ Lab03.sql
@@ -14,14 +14,14 @@ From Student
 Where sID < 300;
 ---
 --- Output:
-+------+-------+------+
-| sID | sName | gpa |
-+------+-------+------+
-| 123 | Amy  | 3.9 |
-| 234 | Bob  | 3.6 |
-+------+-------+------+
-2 rows in set (0.00 sec)
++------+--------+------+
+| sID  | sName  | gpa  |
++------+--------+------+
+| 123  | Amy    | 3.9  |
+| 234  | Bob    | 3.6  |
++------+--------+------+
 ---
+  
 ----- Question 2: Write a query that displays all of the unique student names, college names, and GPAs. 
 ----- Submit both the query and the results. Submit the query and the results from running the query.
 
@@ -32,22 +32,59 @@ Where Student.sID = Apply.sID;
 ---
 --- Output: 
 +-------+----------+------+
-| sName | cName  | gpa |
+| sName | cName    | gpa  |
 +-------+----------+------+
-| Amy  | Stanford | 3.9 |
-| Amy  | Berkeley | 3.9 |
-| Amy  | Cornell | 3.9 |
-| Bob  | Berkeley | 3.6 |
-| Craig | MIT   | 3.5 |
-| Craig | Cornell | 3.5 |
-| Fay  | Stanford | 3.8 |
-| Helen | Stanford | 3.7 |
-| Helen | Berkeley | 3.7 |
-| Irene | Stanford | 3.9 |
-| Irene | MIT   | 3.9 |
-| Jay  | Stanford | 2.9 |
-| Jay  | Cornell | 2.9 |
-| Craig | MIT   | 3.4 |
+| Amy   | Stanford | 3.9  |
+| Amy   | Berkeley | 3.9  |
+| Amy   | Cornell  | 3.9  |
+| Bob   | Berkeley | 3.6  |
+| Craig | MIT      | 3.5  |
+| Craig | Cornell  | 3.5  |
+| Fay   | Stanford | 3.8  |
+| Helen | Stanford | 3.7  |
+| Helen | Berkeley | 3.7  |
+| Irene | Stanford | 3.9  |
+| Irene | MIT      | 3.9  |
+| Jay   | Stanford | 2.9  |
+| Jay   | Cornell  | 2.9  |
+| Craig | MIT      | 3.4  |
 +-------+----------+------+
-14 rows in set (0.00 sec)
 ---
+
+----- Question 3: What are the GPAs of students in the database that applied to Stanford with a major in CS and has a sizeHS of 
+----- less than 900? Submit the query and the results from running the query.
+
+--- Input: 
+SELECT GPA
+FROM Student, Apply
+WHERE Student.sID = Apply.sID and sizeHS < 900 and major = 'CS' and cName = 'Stanford';
+---
+--- Output:
++------+
+| GPA  |
++------+
+| 3.7  |
+| 3.9  |
++------+
+---
+
+----- Question 4: What are the sIDs, sNames, GPA, cNames, and state for all students in the database with 
+----- a GPA greater than 3.75 that applied to a college in the state of CA? Display only distinct values. 
+----- Submit the query and the results from running the query.
+
+--- Input:
+Select distinct S.sID, sName, GPA, C.cName, state
+From Student S, College C, Apply A
+Where S.sID = A.sID and C.cName = A.cName and GPA > 3.75 and state = 'CA';
+---
+--- Output: 
++------+-------+------+----------+-------+
+| sID  | sName | GPA  | cName    | state |
++------+-------+------+----------+-------+
+| 123  | Amy   | 3.9  | Berkeley | CA    |
+| 123  | Amy   | 3.9  | Stanford | CA    |
+| 678  | Fay   | 3.8  | Stanford | CA    |
+| 876  | Irene | 3.9  | Stanford | CA    |
++------+-------+------+----------+-------+
+---
+
